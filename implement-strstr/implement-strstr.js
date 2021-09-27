2023337f5d110fee64c8469cbf3384f2c5cd99d6
Alisha Edington <77764264+alishaedington@users.noreply.github.com>
2021-09-27 17:18:36 -0600

    Time: 72 ms (90.35%), Space: 39.9 MB (74.71%) - LeetHub

---
+++ implement-strstr/implement-strstr.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    if (haystack || needle) { return haystack.indexOf(needle); }
+    return 0;
+};
