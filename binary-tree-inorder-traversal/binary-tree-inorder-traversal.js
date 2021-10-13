a925e864030e06f62b20abb0590041ab34b908c8
Alisha Edington <alisha_56789@yahoo.com>
2021-10-13 12:37:33 -0600

    Time: 117 ms (11.09%), Space: 39.6 MB (11.59%) - LeetHub

---
+++ binary-tree-inorder-traversal/binary-tree-inorder-traversal.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root) {
+    let result = [];
+    if (root) {
+        if (root.left) { result = result.concat(inorderTraversal(root.left)); }
+        result.push(root.val);
+        if (root.right) { result = result.concat(inorderTraversal(root.right)); }
+    }
+    return result;
+};
